e710d06d7216ae1f4535ef9ba080e0e61fd6630c
Rahul Senapati <rahulsen@buffalo.edu>
2025-12-28 02:41:32 -0500

    vite config updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({
   base: "/react-gh-pages",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()]
 }));

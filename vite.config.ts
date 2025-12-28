5bcf203d1fdc4350535a34feb791b992198e5d14
Rahul Senapati <rahulsen@buffalo.edu>
2025-12-28 04:04:47 -0500

    vite.config dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { componentTagger } from "lovable-tagger";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build:{
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html')
       },
     },
     watch: {

41386404e49fa39739109d334d5193a57f9cc19b
Rahul Senapati <rahulsen@buffalo.edu>
2025-12-28 02:44:10 -0500

    update App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({
+export default defineConfig(({ mode }) => ({
   base: "/react-gh-pages",
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));

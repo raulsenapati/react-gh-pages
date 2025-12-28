d54f15ba299014ee9ee58cc4a89fa01ccc569817
Rahul Senapati <rahulsen@buffalo.edu>
2025-12-28 17:27:30 -0500

    redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/react-gh-pages",
+  base: "/react-gh-pages/",
   server: {
     open: true,
     host: "::",
